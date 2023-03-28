89071c6e6e6fc9673e5d0dd071d7978d62ffa54f
majing15134 <majing15134@autohome.com.cn>
2023-03-28 19:39:01 +0800

    添加StringFormat

--- .cpp_template.cpp
+++ .cpp_template.cpp
@@ -1,41 +1,8 @@
 #include <string>
 #include <iostream>
-using std::string;
-
-static string get_channel_value() {
-
-    const char* str_val = "[16719443274]";
-    string val = str_val != nullptr ? str_val : "";
-    if (val[0] == '[' && val[val.length() - 1] == ']') {
-        val = val.substr(1, val.length() - 2);
-    }
-    return val;
-}
-
-static string get_line_parms() {
-
-    string val = get_channel_value();
-    std::cout << val << std::endl;
-
-    string sip_network_ip = "1";
-    string sip_network_port = "2";
-
-    string line_addr = sip_network_ip + ":" + sip_network_port;
-    std::cout << line_addr << std::endl;
-
-    // string sip_to_user = val;
-    // string callee_prefix;
-    // if (sip_to_user.length() > 11) {
-    //     callee_prefix = sip_to_user.substr(0, sip_to_user.length() - 11);
-    //     sip_to_user = sip_to_user.substr(sip_to_user.length() - 11, 11);
-    // }
-    
-    //std::cout <<  "callee_prefix: " << callee_prefix << ", callee_id: " << sip_to_user << std::endl;
-
-    return "";
-}
+using namespace std;
 
 int main() {
-    get_line_parms();
+    
     return 0;
 }
---
+++ vsprintf_test.cpp
@@ -0,0 +1,74 @@
+/**
+ * @file vsprintf_test.cpp
+ * @author xiao2macf
+ * @brief Use vsprintf to implement StringFormat
+ * @version 0.1
+ * @date 2023-03-28
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <string>
+#include <iostream>
+#include <stdarg.h> //va_start
+#include <assert.h> //assert
+#include <string.h> //strlen
+using namespace std;
+
+string StringFormat(const char *fmt, ...)
+{
+    va_list argptr;
+    va_start(argptr, fmt);
+    int len = vsnprintf(nullptr, 0, fmt, argptr);
+    assert(len != 0);
+    string str(len, '\0');
+    int cnt = vsprintf((char*)str.data(), fmt, argptr);
+    va_end(argptr);
+    return std::move(str);
+}
+
+int main() {
+
+    cout << StringFormat("Hello World! Hello %d", 100)  << endl;
+
+    string str = "Hello World! Hello 100";
+    // cout << __FUNCTION__ << "# str.size()=" << str.size() << ", str.length()=" << str.length() << endl;
+    // cout << __FUNCTION__ << "# str=" << str << endl;
+    // cout << __FUNCTION__ << "# strlen(str.c_str())=" << strlen(str.c_str()) << endl;
+    // cout << __FUNCTION__ << "# str.max_size()=" << str.max_size() << endl;
+    // cout << __FUNCTION__ << "# sizeof(str)=" << sizeof(str) << endl;
+    
+    return 0;
+}
+
+/**
+Hello World! Hello 100
+main# str.size()=22, str.length()=22
+main# str=Hello World! Hello 100
+main# strlen(str.c_str())=22
+main# str.max_size()=9223372036854775807
+main# sizeof(str)=32
+ * 
+ */
+ 
+// string StringFormat(const char *fmt, ...)
+// {
+//     va_list argptr;
+//     va_start(argptr, fmt);
+//     va_list args2;
+//     va_copy(args2, argptr);
+//     cout << fmt << endl;
+//     int len = vsnprintf(nullptr, 0, fmt, argptr);
+//     cout << len << endl;
+//     assert(len != 0);
+//     string str(len, '\0');
+
+//     int cnt = vsprintf((char*)str.data(), fmt, argptr);
+//     va_end(argptr);
+
+//     cout << __FUNCTION__ << "# len=" << len << ", cnt=" << cnt << ", str.size()=" << str.size() << ", str.length()=" << str.length() << endl;
+//     cout << __FUNCTION__ << "# str=" << str << endl;
+//     cout << __FUNCTION__ << "# strlen(str.c_str())=" << strlen(str.c_str()) << endl;
+//     cout << __FUNCTION__ << "# str.capacity()=" << str.capacity() << endl;
+//     return std::move(str);
+// }
